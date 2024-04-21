0e4741aa29c4acb781576f0e5eaf19be823dcb11
Julio Fernandez <jfvilas@outlook.com>
2024-04-21 19:57:31 +0200

    cl

--- publish-authorizator-to-docker-hub.cmd
+++ publish-authorizator-to-docker-hub.cmd
@@ -2,7 +2,7 @@ call npm run build
 set /p major=<major
 set /p minor=<minor
 set /p level=<level
-set currversion=%major%.%minor%.%level%
+set currentversion=%major%.%minor%.%level%
 docker image rm obk-authorizator:latest
 docker build . -t obk-authorizator -t jfvilasoutlook/obk-authorizator:%currentversion% -t jfvilasoutlook/obk-authorizator:latest
 docker push jfvilasoutlook/obk-authorizator:%currentversion%
